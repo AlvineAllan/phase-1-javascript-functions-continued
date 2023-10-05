f4ad215a97c9b5156662d1fed53d7fc47c1cadc9
Alvine Allan <alvizallaz@gmail.com>
2023-10-05 11:30:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(rein="roller-skate"){
+     return `This Saturday, I want to ${rein}!`
+}
+function mondayWork (rein = "go to the office"){
+    return `This Monday, I will ${rein}.`
+}
+function wrapAdjective(star = "*"){  
+    return function( stu ="special"){
+         return(`You are ${star}${stu}${star}!`)
+    }
+}
